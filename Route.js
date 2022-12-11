5f2c557fd47f20528304203282ba2c350be926c4
Dubois Hugo <195347@ecam.be>
2022-12-11 16:36:04 +0100

    (#2) Routes updates, cleaning up the route.js file

--- Route.js
+++ Route.js
@@ -6,86 +6,14 @@ let router = express.Router();
 
 let FlightFunctions = require('./FlightFunctions.js');
 
-
-/*
-    * Flight avec la route et les deux aéroports
-    * Un plane avec tout le plane type et la ompagnie
-    * Garder la convention du 'J'
-*/
-/*
-
-    - GET the flight by ID
-    - GET the route by ID 
-    - GET the flightnumber 
-    - GET the departure time 
-    - GET the plane
-    - GET the terminal
-
- */
-
+// Request for the flight that contains also the route, the departure and the arrival airports
 router.get('/flight/:flightnumber', FlightFunctions.getTheFlight);
 
-//router.get('/flight/flightroute/:flightnumber', FlightFunctions.getTheFlightRoute);
-
-//router.get('/flight/flightnumber/:flightnumber', FlightFunctions.getTheFlightNumber);
-
+// Request for the plane that also contains the plane-type and the company
 router.get('/flight/departuretime/:time', FlightFunctions.getTheFlightDeparture);
 
-//router.get('/flight/plane/:flightnumber', FlightFunctions.getTheFlightPlane);
-
-//router.get('/flight/terminal/:flightnumber', FlightFunctions.getTheFlightPlaneTerminal);
-
-
-/*
-
-    - GET the route by ID
-    - GET the route's departure airport by name
-    - GET the route's arrival airport by name
-    - GET the route's flight time by ID
-
-*/
-/*
-router.get('/route/:routeid', FlightFunctions.getTheRoute);
-
-router.get('/route/departureairport/:routeid', FlightFunctions.getTheRouteDeparture);
-
-router.get('/route/arrivalairport/:routeid', FlightFunctions.getTheRouteArrivalAirport);
-
-router.get('/route/flighttime/:routeid', FlightFunctions.getTheRouteFlightTime);
-*/
-
-
-/*
-
-    - GET the plane by ID
-    - GET the company
-    - GET the type of the plane
-
-*/
-
+// Request for the plane that also contains the plane-type and the company
 router.get('/plane/:planenumber', FlightFunctions.getThePlane);
-
-//router.get('/plane/company/:planenumber', FlightFunctions.getThePlaneCompany);
-
-//router.get('/plane/type/:planenumber', FlightFunctions.getThePlaneType);
-
-
-
-/*
-
-    - GET an airport by ID
-    - GET the name
-    - GET the number of terminals
-
-*/
-
-/*
-router.get('/airport/:airportacronym', FlightFunctions.getTheAirport);
-
-router.get('/airport/name/:airportacronym', FlightFunctions.getTheAirportName);
-
-router.get('/airport/terminalnumber/:airportacronym', FlightFunctions.getTheAirportTerminalNumber);
-*/
 
 //Export router to use it on server.js
 module.exports = router;
